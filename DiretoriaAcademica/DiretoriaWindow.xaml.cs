527574bdb03e99ee23a3275305a884b282c5fef9
kuabrs <95240062+kuabrs@users.noreply.github.com>
2023-01-30 15:46:14 -0300

    Adicionei Tarefa 2

---
+++ DiretoriaAcademica/DiretoriaWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DiretoriaAcademica
+{
+    /// <summary>
+    /// Lógica interna para DiretoriaWindow.xaml
+    /// </summary>
+    public partial class DiretoriaWindow : Window
+    {
+        public DiretoriaWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DiretoriaAcademica/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiretoriaAcademica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Diretoria_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Professor_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistroP_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistroC_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListaC_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Curso_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListaP_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
